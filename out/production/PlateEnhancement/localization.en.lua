cfdde1b05abee5259d35df52bcc048b7108917d8
kira1101 <kira1101@hotmail.com>
2020-11-13 13:20:13 +0800

    first commit

---
+++ out/production/PlateEnhancement/localization.en.lua
@@ -0,0 +1,2 @@
+POKER_BROWSER_TITLE = "Texas Hold'em Table Browser"
+POKER_BROWSER_CLOSE = "Close"
---
+++ src/enus.lua
@@ -0,0 +1,6 @@
+
+local L = LibStub("AceLocale-3.0"):NewLocale("PlateEnhancement", "enUS", true)
+L["Plate Enhancement"] = "Plate Enhancement"
+L["General Settings"] = "General Settings"
+L["Player auras"] = "Player auras"
+L["author"] = "author"
